9524f3545010466e357d9f97a2884435f5ba5223
rahulsanju <rahulmora007@gmail.com>
2024-02-06 23:30:10 -0500

    State Design Pattern

---
+++ Behavioral/state_pattern/LaunchScreenState.java
@@ -0,0 +1,12 @@
+package Behavioral.state_pattern;
+
+public class LaunchScreenState implements State{
+    @Override
+    public String doAction(Game game) {
+        System.out.println("#####\n\n");
+        System.out.println("LaunchingScreen:");
+        System.out.println("Welcome!!");
+        System.out.println("\n\n#####");
+        return "MainMenuScreen";
+    }
+}
---
+++ Behavioral/state_pattern/State.java
@@ -0,0 +1,5 @@
+package Behavioral.state_pattern;
+
+public interface State {
+    String doAction(Game game);
+}
